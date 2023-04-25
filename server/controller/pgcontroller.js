43ee9995dbf01f69f31894299447671e94938109
sunjin <sjink@pm.me>
2023-04-25 19:24:57 -0400

    completed db crud routes

---
+++ server/controller/pgcontroller.js
@@ -0,0 +1,90 @@
+const pg = require('pg');
+const conString = 'postgres://ebxjbhom:EgqqfRwi12To3bnGu7iwqgGKTPnQ6p_W@lallah.db.elephantsql.com/ebxjbhom';
+
+const pgController = {};
+
+pgController.createTable = (req, res, next) => {
+  // const response = await client.query('CREATE TABLE $1 ')
+};
+
+pgController.createTask = (req, res, next) => {
+  const {
+    task,
+    description
+  } = req.body;
+  const params = [task, description];
+  client.query('INSERT INTO todo (task, description) VALUES ($1, $2);', params, (err, result) => {
+    if (err) {
+      return next({
+        log: 'Express error handler in createTask',
+        status: 500,
+        message: { err: 'An error occurred' }
+      });
+    } else {
+      return next();
+    }
+  });
+};
+pgController.readTable = (req, res, next) => {
+  client.query('SELECT (task, description) FROM todo', (err, result) => {
+    if (err) {
+      return next({
+        log: 'Express error handler in readTask',
+        status: 500,
+        message: { err: 'An error occurred' }
+      });
+    } else {
+      res.locals.response = result.rows;
+      return next();
+    }
+  });
+};
+pgController.updateTask = (req, res, next) => {
+  const {
+    task,
+    description
+  } = req.body;
+  const {
+    id
+  } = req.params;
+  const params = [id, task, description];
+  client.query('UPDATE todo SET task = $2, description = $3 WHERE task_id = $1;', params, (err, result) => {
+    if (err) {
+      return next({
+        log: 'Express error handler in updateTask',
+        status: 500,
+        message: { err: 'An error occurred' }
+      });
+    } else {
+      return next();
+    }
+  });
+};
+pgController.deleteTask = (req, res, next) => {
+  const {
+    id
+  } = req.params;
+  const params = [id];
+  client.query('DELETE FROM todo WHERE task_id = $1;', params, (err, result) => {
+    if (err) {
+      return next({
+        log: 'Express error handler in deleteTask',
+        status: 500,
+        message: { err: 'An error occurred' }
+      });
+    } else {
+      return next();
+    }
+  });
+};
+
+const client = new pg.Client(conString);
+client.connect((err) => {
+  if (err) {
+    console.error('connection error', err.stack);
+  } else {
+    console.log('client connected');
+  }
+});
+
+module.exports = pgController;
---
+++ server/server.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+const pgController = require('./controller/pgcontroller');
+
+app.use(express.json());
+// app.get('/', (req, res) => {
+//   res.json('Testing');
+// });
+
+app.post('/', pgController.createTask, (req, res) => {
+  res.json(' created task');
+});
+
+app.get('/', pgController.readTable, (req, res) => {
+  res.json(res.locals.response);
+});
+
+app.put('/update/:id', pgController.updateTask, (req, res) => {
+  res.json('updated task');
+});
+app.delete('/delete/:id', pgController.deleteTask, (req, res) => {
+  res.json(' deleted task');
+});
+
+app.use((err, req, res, next) => {
+  const defaultErr = {
+    log: 'Express error handler caught unknown middleware error',
+    status: 500,
+    message: { err: 'An error occurred' }
+  };
+  const errorObj = Object.assign({}, defaultErr, err);
+  console.log(errorObj.log);
+  return res.status(errorObj.status).json(errorObj.message);
+});
+
+app.listen(port, () => {
+  console.log('Listening on port ', port);
+});
